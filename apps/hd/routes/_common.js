8e12fee94dd6d06addbddb0443391d62a8c218ea
godengine <1027157090@qq.com>
2018-07-18 20:34:26 +0800

    fix:

--- apps/hd/routes/_common.js
+++ apps/hd/routes/_common.js
@@ -9,11 +9,11 @@ const tools = req('libs/utils')
 const moment = require('moment')
 const interUtile = require('../libs/internationlUtils@0.1.0.js')
 const isPro = process.env.NODE_ENV === 'production'
-const APP_NAME = '2018-card'
+const APP_NAME = 'marry'
 
 module.exports = (router, logger) => {
   let log = tools.loggers(logger)
-  router.get(`/${APP_NAME}`, function * (next){
+  router.get(`/${APP_NAME}`, function * (next) {
     try {
       yield this.render(APP_NAME)
       yield next
